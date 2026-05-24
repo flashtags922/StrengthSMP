880899fb89d17169cf0a02d604502065fb648458
flashtags922 <louillalim.85@gmail.com>
2026-05-25 02:34:05 +0800

    Update StrengthSMP.java

--- src/main/java/me/anti/strength/StrengthSMP.java
+++ src/main/java/me/anti/strength/StrengthSMP.java
@@ -8,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -25,10 +24,6 @@ public class StrengthSMP extends JavaPlugin implements Listener {
             "sword", "axe", "trident", "bow", "crossbow", "shield"
     };
 
-    private final String[] WEAPONS = {
-            "sword", "axe", "trident", "bow", "crossbow", "shield"
-    };
-
     @Override
     public void onEnable() {
         saveDefaultConfig();
@@ -39,54 +34,32 @@ public class StrengthSMP extends JavaPlugin implements Listener {
     }
 
     // =========================
-    // PLAYER JOIN SYSTEM
-    // =========================
-   @EventHandler
-public void onJoin(PlayerJoinEvent event) {
-
-    Player player = event.getPlayer();
-    String uuid = player.getUniqueId().toString();
-
-    if (!getConfig().isSet(uuid + ".class")) {
-        getConfig().set(uuid + ".class", "none");
-        getConfig().set(uuid + ".strength", getConfig().getInt("def_strength"));
-        saveConfig();
-    }
-
-    String weapon = getConfig().getString(uuid + ".class", "none");
-    int strength = getConfig().getInt(uuid + ".strength");
-
-    player.sendMessage("§a====================");
-    player.sendMessage("§6Strength: §e" + strength + "+");
-    player.sendMessage("§6Weapon: §e" + weapon.toUpperCase());
-    player.sendMessage("§a====================");
-}
-
-    // =========================
-    // DEATH = -STRENGTH SYSTEM
+    // JOIN SYSTEM
     // =========================
     @EventHandler
-    public void onDeath(PlayerDeathEvent event) {
+    public void onJoin(PlayerJoinEvent event) {
 
-        Player player = event.getEntity();
+        Player player = event.getPlayer();
         String uuid = player.getUniqueId().toString();
 
-        int min = getConfig().getInt("min_strength");
-
-        int current = getConfig().getInt(uuid + ".strength", 0);
-        boolean naturalLoss = getConfig().getBoolean("natural_cause_lose_strength");
-
-        if (naturalLoss) {
-            current -= 1;
-            if (current < min) current = min;
-
-            getConfig().set(uuid + ".strength", current);
+        // create default data
+        if (!getConfig().isSet(uuid + ".class")) {
+            getConfig().set(uuid + ".class", "none");
+            getConfig().set(uuid + ".strength", getConfig().getInt("def_strength"));
             saveConfig();
         }
+
+        String weapon = getConfig().getString(uuid + ".class", "none");
+        int strength = getConfig().getInt(uuid + ".strength");
+
+        player.sendMessage("§a====================");
+        player.sendMessage("§6Strength: §e" + strength + "+");
+        player.sendMessage("§6Weapon: §e" + weapon.toUpperCase());
+        player.sendMessage("§a====================");
     }
 
     // =========================
-    // REROLL SYSTEM
+    // REROLL SYSTEM (CHANGES WEAPON = CLASS)
     // =========================
     @EventHandler
     public void onUse(PlayerInteractEvent event) {
@@ -107,18 +80,13 @@ public void onJoin(PlayerJoinEvent event) {
         String uuid = player.getUniqueId().toString();
 
         String rolledClass = CLASSES[random.nextInt(CLASSES.length)];
-        String rolledWeapon = WEAPONS[random.nextInt(WEAPONS.length)];
 
         getConfig().set(uuid + ".class", rolledClass);
-        getConfig().set(uuid + ".weapon", rolledWeapon);
         saveConfig();
 
         item.setAmount(item.getAmount() - 1);
 
-        player.sendMessage("§6====================");
-        player.sendMessage("§eNEW CLASS: §f" + rolledClass.toUpperCase());
-        player.sendMessage("§eNEW WEAPON: §f" + rolledWeapon.toUpperCase());
-        player.sendMessage("§6====================");
+        player.sendMessage("§6You rolled weapon: §f" + rolledClass.toUpperCase());
     }
 
     // =========================
@@ -131,7 +99,7 @@ public void onJoin(PlayerJoinEvent event) {
 
         meta.setDisplayName(ChatColor.GREEN + "Reroll Guide");
         meta.setLore(Arrays.asList(
-                ChatColor.GRAY + "Right click to reroll class",
+                ChatColor.GRAY + "Right-click to reroll weapon",
                 ChatColor.DARK_GRAY + "Consumes on use"
         ));
 
